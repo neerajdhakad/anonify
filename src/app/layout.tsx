14f11aa0d295a36762a3d9d3b66a09cfc1f5b4da
NEERAJ DHAKAD <dhakadneeraj786@gmail.com>
2024-05-05 23:16:14 +0530

    first-commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { AuthProvider } from "@/context/AuthProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Anonify",
+  description: "An application for sending message to anonymous user",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <AuthProvider>
+        <body className={inter.className}>{children}</body>
+      </AuthProvider>
+    </html>
+  );
+}
---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const usernameValidation = z
+  .string()
+  .min(2, "Username must be of atleast 2 characters")
+  .max(2, "Username must be not more than 20 characters")
+  .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters");
+
+export const signUpSchema = z.object({
+  username: usernameValidation,
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "password must be at least 6 characters" }),
+});
